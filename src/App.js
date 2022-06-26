459aec7ec40ee8ee74ba119fbf8ebf7f4505583c
byf1n <kirillozubenko@gmail.com>
2022-06-26 13:39:37 +0200

    hm2_users dome

--- src/App.js
+++ src/App.js
@@ -5,10 +5,7 @@ function App() {
   return (
     <div>
         <Users/>
-        є API от SpaceX
-        https://api.spacexdata.com/v3/launches/
-        потрібно вивести всі запуски кораблів окрім 2020 року
-        репрезентувати тільки окремі поля (зазначені в скрнішоті в папці)
+
     </div>
   );
 }
